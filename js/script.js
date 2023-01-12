67aaa17f710ea20781e5ce5535b3f1baf0d3ce22
erosmartz <107828862+erosmartz@users.noreply.github.com>
2023-01-12 19:00:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,53 +1,66 @@
-
-
 let array_juegos = [];
 let contador = 0;
 
-function set_datos(){
+function data_push() {
+  /* FUNCION PUSH A JSON */
+  array_juegos.push(juego);
+  let array_JSON = JSON.stringify(array_juegos);
 
-    let data_nombre = document.getElementById("input_nombre");
-    let data_precio = document.getElementById("input_precio");
+  localStorage.setItem("array_RAW", array_JSON);
 
-    console.log(data_nombre.value);
-    console.log(data_precio.value);
+  let restore_juego = localStorage.getItem("array_RAW");
 
-    let juego = {Nombre:data_nombre.value , Precio:data_precio.value};
-    
-    array_juegos.push( juego );
-    let array_JSON = JSON.stringify(array_juegos);
+  console.log(restore_juego);
 
-    localStorage.setItem("array_RAW" , array_JSON);
+  restore_juego = JSON.parse(restore_juego);
 
+  console.log(restore_juego);
 
-    let restore_juego = localStorage.getItem("array_RAW");
-
-    console.log(restore_juego);  
-
-    restore_juego = JSON.parse( restore_juego);
-
-    console.log(restore_juego);
-
-
-
+  /* FUNCION PUSH A JSON */
 }
 
-function pop_up() {
+function set_datos() {
+  let data_nombre = document.getElementById("input_nombre");
+  let data_precio = document.getElementById("input_precio");
 
+  console.log(data_nombre.value);
+  console.log(data_precio.value);
 
-    let data_precio_masiva = parseFloat(data_precio.value) * 1.75;
+  /* OBJETO */
+  let juego = { Nombre: data_nombre.value, Precio: data_precio.value };
 
-    let seccion = document.getElementById("formulario");
-    let datos = "<div id='" + contador + "' class='block box'><p>" + "El juego '" + data_nombre.value + "' de $" + data_precio.value + " con impuestos vale unos: " + "$" + data_precio_masiva + " Pesos Argentinos." + "<div class='control'><button class='button is-link' id='input_addtocart'>Agregar a Lista de Deseados</button></div></div>";
-    let info_juego = document.createElement("div");
+  let data_precio_total = parseFloat(juego.Precio) * 1.75;
+
+  let node_check = document.getElementById("data_node");
+  console.log(node_check);
+
+  let seccion = document.getElementById("formulario");
+
+  if (node_check == null) {
     
+    let datos =
+      "<div id='data_node' class='block box'><p>" +
+      "El juego '" +
+      juego.Nombre +
+      "' de $" +
+      juego.Precio +
+      " con impuestos vale unos: " +
+      "$" +
+      data_precio_total +
+      " Pesos Argentinos." +
+      "<div class='control'><button class='button is-link' id='input_addcart'>Agregar a Lista de Deseados</button></div></div>";
+    let info_juego = document.createElement("div");
+
     info_juego.innerHTML = datos;
     seccion.append(info_juego);
+  }
+  
+  
+  else if (node_check != null) {
 
+  }
 }
-
 
 let input_send = document.getElementById("input_send");
 
-input_send.addEventListener("click",set_datos);
-input_send.addEventListener("click",pop_up);
-
+input_send.addEventListener("click", set_datos);
