1cd047a85be1a792e9a942d034b43bfb8e9c7443
erosmartz <107828862+erosmartz@users.noreply.github.com>
2023-01-12 20:54:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 let array_juegos = [];
 let contador = 0;
+let juego_global;
 
 function data_push() {
   /* FUNCION PUSH A JSON */
-  array_juegos.push(juego);
+  array_juegos.push(juego_global);
   let array_JSON = JSON.stringify(array_juegos);
 
   localStorage.setItem("array_RAW", array_JSON);
 
   let restore_juego = localStorage.getItem("array_RAW");
 
-  console.log(restore_juego);
 
   restore_juego = JSON.parse(restore_juego);
 
@@ -23,44 +23,39 @@ function set_datos() {
   let data_nombre = document.getElementById("input_nombre");
   let data_precio = document.getElementById("input_precio");
 
-  console.log(data_nombre.value);
-  console.log(data_precio.value);
-
   /* OBJETO */
   let juego = { Nombre: data_nombre.value, Precio: data_precio.value };
 
-  let data_precio_total = parseFloat(juego.Precio) * 1.75;
-
   let node_check = document.getElementById("data_node");
-  console.log(node_check);
 
   let seccion = document.getElementById("formulario");
 
-  if (node_check == null) {
-    
-    let datos =
-      "<div id='data_node' class='block box'><p>" +
-      "El juego '" +
-      juego.Nombre +
-      "' de $" +
-      juego.Precio +
-      " con impuestos vale unos: " +
-      "$" +
-      data_precio_total +
-      " Pesos Argentinos." +
-      "<div class='control'><button class='button is-link' id='input_addcart'>Agregar a Lista de Deseados</button></div></div>";
+  if (node_check) {
+    let info_juego = document.getElementById("data_node");
+    info_juego.innerHTML = `<p>El juego "${juego.Nombre}" 
+    de $${juego.Precio}, 
+    con todos los impuestos sale $${juego.Precio * 1.75}.
+    <div class="control"><button class="button is-link" id="input_addcart">Agregar a Deseados</button></div>`;
+
+  } else {
+
     let info_juego = document.createElement("div");
 
-    info_juego.innerHTML = datos;
-    seccion.append(info_juego);
-  }
-  
-  
-  else if (node_check != null) {
+    info_juego.innerHTML = `<div id="data_node" class="box"><p>El juego "${juego.Nombre}" 
+    de $${juego.Precio}, con todos los impuestos sale $${juego.Precio * 1.75}.
+    <div class="control"><button class="button is-link" id="input_addcart">Agregar a Deseados</button></div></div>`;
 
+    seccion.append(info_juego);
+    let input_add = document.getElementById("input_addcart");
+    input_add.addEventListener("click",data_push);
   }
+  juego_global = juego;
 }
 
-let input_send = document.getElementById("input_send");
 
+let input_send = document.getElementById("input_send");
 input_send.addEventListener("click", set_datos);
+
+
+
+
